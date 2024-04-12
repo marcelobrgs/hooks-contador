0e73b469cf08ad9b81db7f7a5472c537707627ab
marcelobrgs <marcelo.borges@outlook.com>
2024-04-12 09:27:45 -0300

    first commig

---
+++ src/HooksApp.jsx
@@ -0,0 +1,10 @@
+import { ContadorComponent } from "./components/ContadorComponent";
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>HooksApp</h1>
+      <hr></hr>
+      <ContadorComponent></ContadorComponent>
+    </>
+  );
+};
